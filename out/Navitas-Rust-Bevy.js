15bd487c500087ac82c8f2b38cb17836d7c3149c
zednaked <zednaked@gmail.com>
2024-05-12 17:16:35 -0300

    mudanças feitas no mb

--- out/Navitas-Rust-Bevy.js
+++ out/Navitas-Rust-Bevy.js
@@ -2022,7 +2022,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('Hectares_bg.wasm', import.meta.url);
+        input = new URL('Navitas-Rust-Bevy_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
